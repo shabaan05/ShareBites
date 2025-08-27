ec1b8f4906cef333e35d9047bedc15b87c51a116
shabaan05 <shabaansatarkar2003@gmail.com>
2025-08-27 16:06:35 +0530

    add commit

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import indexRoutes from "./routes/index.js";
-
 import { fileURLToPath } from "url";
+
 dotenv.config();
 
 const app = express();
@@ -14,19 +14,17 @@ const PORT = process.env.PORT || 3000;
 import connectDB from "./config/db.js";
 connectDB(); 
 
-
+//............................................................
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public"))); // serve public folder
-
 // Set EJS as view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+//............................................................
 
 // routes 
 app.use("/", indexRoutes);
--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,13 @@
+// src/config/db.js
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI); // from .env
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // stop server if DB fails
   }
 };
 
